50b876dc7cd9bd43cf17edd999f1b56f6bdcb7a4
Samuel Issadore <94148426+theutisticcoder@users.noreply.github.com>
2026-01-17 20:39:23 -0500

    fix ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: 3000,
-        allowedHosts: 'all',
+        host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
